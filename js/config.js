ec241a4dd14e43d80dd7a514a1d4dd5b8e5007c0
太極 <134386918+taichicoin@users.noreply.github.com>
2026-04-12 04:40:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,3 +2,4 @@
 const SUPABASE_URL = 'https://sznjaotjoljaiawbvfro.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_iN3D88OfHeUre4ddCaDH7g_rlsQ8LGN';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+console.log('✅ Supabase 客户端已初始化');
