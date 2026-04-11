4e19535f82fa77a5f64480b79f0cb003d82c17ab
太極 <134386918+taichicoin@users.noreply.github.com>
2026-04-12 03:52:45 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// Supabase 配置
+const SUPABASE_URL = 'https://sznjaotjoljaiawbvfro.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_iN3D88OfHeUre4ddCaDH7g_rlsQ8LGN';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
